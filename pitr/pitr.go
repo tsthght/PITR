931e9b61107745bc70099ff7d4d05151ed963e24
WangXiangUSTC <xiang13225080@163.com>
2019-10-15 19:25:14 +0800

    remove useless code

--- pitr/pitr.go
+++ pitr/pitr.go
@@ -48,16 +48,6 @@ func (r *PITR) Process() error {
 		}
 
 		log.Info("process", zap.Reflect("binlog", binlog))
-		/*
-			ignore, err := filterBinlog(r.filter, binlog)
-			if err != nil {
-				return errors.Annotate(err, "filter binlog failed")
-			}
-
-			if ignore {
-				continue
-			}
-		*/
 	}
 }
 
@@ -65,44 +55,6 @@ func (r *PITR) Process() error {
 func (r *PITR) Close() error {
 	return nil
 }
-
-/*
-// may drop some DML event of binlog
-// return true if the whole binlog should be ignored
-func filterBinlog(afilter *filter.Filter, binlog *pb.Binlog) (ignore bool, err error) {
-	switch binlog.Tp {
-	case pb.BinlogType_DDL:
-		var table filter.TableName
-		_, table, err = parseDDL(string(binlog.GetDdlQuery()))
-		if err != nil {
-			return false, errors.Annotatef(err, "parse ddl: %s failed", string(binlog.GetDdlQuery()))
-		}
-
-		if afilter.SkipSchemaAndTable(table.Schema, table.Table) {
-			return true, nil
-		}
-
-		return
-	case pb.BinlogType_DML:
-		var events []pb.Event
-		for _, event := range binlog.DmlData.GetEvents() {
-			if afilter.SkipSchemaAndTable(event.GetSchemaName(), event.GetTableName()) {
-				continue
-			}
-
-			events = append(events, event)
-		}
-
-		binlog.DmlData.Events = events
-		if len(events) == 0 {
-			ignore = true
-		}
-		return
-	default:
-		return false, errors.Errorf("unknown type: %d", binlog.Tp)
-	}
-}
-*/
 
 func isAcceptableBinlog(binlog *pb.Binlog, startTs, endTs int64) bool {
 	return binlog.CommitTs >= startTs && (endTs == 0 || binlog.CommitTs <= endTs)
